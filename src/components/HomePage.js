88fd79c7543a033202891c7edeb11a1f952a0d46
Jorakasi <gabor.olah0323@gmail.com>
2020-09-22 13:17:47 +0200

    created homepage, and navbar

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
+function HomePage() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../style/Navbar.css";
+
+export default function NavBar() {
+    const cookieValue = document.cookie.split("=")[1];
+
+    function removeCookies() {
+        localStorage.clear();
+        let res = document.cookie;
+        let multiple = res.split(";");
+        for (let i = 0; i < multiple.length; i++) {
+            let key = multiple[i].split("=");
+            document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+        }
+    }
+
+    return (
+        <div className="header">
+            <p className="home" href="/" id="logo">
+                Beers :)
+            </p>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
