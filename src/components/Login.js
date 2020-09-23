dc792bac622ca8086b6bc239391971960064925e
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-23 14:33:59 +0200

    adds axios to send a Post request with userLoginData pojo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { LoggedInContext } from "../context/LoggedInContext";
+import Axios from "axios";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(initialState);
+  const [isLoggedIn, setIsLoggedIn] = useState(LoggedInContext);
+
+  const sendUserLoginData = () => {
+    Axios.post("", {
+      username: username,
+      password: password,
+    });
+  };
 
   return <div></div>;
 }
