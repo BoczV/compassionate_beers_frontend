97f6078fe266c9389be703f0cc8404e63da60bdf
TiborGulyas <gulyastibor@gmail.com>
2020-09-23 11:30:04 +0200

    detailed beer styling - partially completed

--- src/components/DetailedBeer.js
+++ src/components/DetailedBeer.js
@@ -1,17 +1,42 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "../style/DetailedBeer.css";
 import { useParams } from "react-router";
+import axios from "axios";
 
-function DetailedBeer() {
+export default function MyProfile() {
+  //const [cookies, setCookie, removeCookie] = useCookies(["auth"]);
+  //const [profileData, setProfileData] = useState([]);
+
   const { beerId } = useParams();
+  const [detailedBeer, setDetailedBeer] = useState({});
 
   useEffect(() => {
     const url = `http://localhost:8762/beerservice/beer/${beerId}`;
     axios.get(url).then((data) => {
       console.log(data.data);
+      setDetailedBeer(data.data);
     });
   }, [beerId]);
-  return <div></div>;
-}
 
-export default DetailedBeer;
+  return (
+    <div className="body-container">
+      <aside class="profile-card">
+        <header>
+          <a href="#">
+            <img src={`${detailedBeer.img_url}`} />
+          </a>
+
+          <h1>"username"</h1>
+
+          <h2>"email"</h2>
+        </header>
+
+        <div class="profile-bio">
+          <p>
+            Under construction... <br /> Please come back later!
+          </p>
+        </div>
+      </aside>
+    </div>
+  );
+}
