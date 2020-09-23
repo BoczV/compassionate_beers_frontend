96cdd78d695560db8c6ebe271a268592c901e3fa
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-23 14:30:34 +0200

    adds Login.js with states and LoggedInContext to track whether the user is logged in or not

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(initialState);
+
+  return <div></div>;
+}
+
+export default Login;
