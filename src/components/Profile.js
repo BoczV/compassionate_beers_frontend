e3c33c86bdcaac6d4a6ba69e3fb6c0363e9a269b
Viktor Bocz <beviktor95@gmail.com>
2020-09-24 18:46:44 +0200

    Profile component mostly done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,54 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import BeerCard from "./BeerCard";
 
 function Profile() {
-  return <div></div>;
+  const [user, setUser] = useState({});
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:8762/profile/${localStorage.getItem(
+      "username"
+    )}`;
+    Axios.get(url).then((data) => {
+      console.log(data.data);
+      setUser(data.data);
+      if (data.data.beerIds) {
+        setBeers(data.data.beerIds);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: "center",
+          display: "block",
+          marginBottom: "-1.5%",
+        }}
+      >
+        {user.userName}'s profile
+      </h1>
+      <p style={{ color: "#002e5b", marginBottom: "2%" }}>
+        E-mail address: {user.email}
+      </p>
+      <h2 style={{ color: "#002e5b" }}>My favorite beers:</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(250px, max-content))",
+          justifyContent: "center",
+          gridGap: "2rem",
+          marginTop: "5%",
+        }}
+      >
+        {beers.map((beer) => (
+          <BeerCard beer={beer} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
